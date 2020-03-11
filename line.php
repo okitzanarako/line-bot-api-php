85a237097198c1437774bc9dc3045168f360ed7e
okitzanarako <62059836+okitzanarako@users.noreply.github.com>
2020-03-11 19:51:26 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'BvDsy8t/wkYCho4aIR/uK1JJquwnnTI7vnenWiu3Df6TEvLoBptYR7HonAtmHUKDQOffAkWetc4iw4Mkw7u4rlMM/VsBB6A5D7DfZ3NjO9kwWKC6wiHm3vvk86eBVCKUyT1nwxoYwLl0ok5n1JxrhQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'YrEHcdRZ0J5RYE9OKNrKAPg9mLaOBOsG7Coq/pZhYV65+SezyMwhmKSa4GUt0GJGar/12FVk6VNZVYfSBynK5NjzW+0iWyML8MYycg+O2YpNsla0mreZ4lJ+PlmuW3NacqlMcAMfpHa1b6DrnWOKTQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
